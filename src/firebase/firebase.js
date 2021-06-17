f6770888b8ff0f0b70692c9bcfe3a72a398c47d3
León Agustín Vila <leonvila98@gmail.com>
2021-06-17 16:46:53 -0300

    adding firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCvksdqhojxT-alU7mJikqEblZraAHBJiU",
+    authDomain: "joke-web.firebaseapp.com",
+    projectId: "joke-web",
+    storageBucket: "joke-web.appspot.com",
+    messagingSenderId: "629674497018",
+    appId: "1:629674497018:web:5b943c139e089314ee4a43",
+    measurementId: "G-0C64L6W6WF"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
